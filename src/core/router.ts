1419d971987663191539279d4aa50d1774d4b452
Javier alfar <dubstepalfaro@gmail.com>
2024-08-25 16:37:22 -0400

    soporte para Rutas Dinámicas

--- src/core/router.ts
+++ src/core/router.ts
@@ -1,75 +1,103 @@
-//router.ts
 import { Request } from "./request";
 import { Response } from "./response";
+import { match, MatchFunction, Path } from "path-to-regexp";
 
 type Method = "GET" | "POST" | "DELETE" | "PUT" | "PATCH";
-
-interface Route {
-  path: string;
-  handler: (req: Request, res: Response) => void;
-}
+type Handler = (req: Request, res: Response) => void;
+type Route = { method: Method; path: string; handler: Handler };
 
 export class Router {
-  private routes: { [key: string]: (req: Request, res: Response) => void } = {};
+  private routes: Route[] = [];
   private basePath: string = "";
+  private middlewares: Handler[] = [];
 
   constructor(basePath: string = "") {
     this.basePath = basePath;
   }
 
-  private addRoute(
-    method: Method,
-    path: string,
-    handler: (req: Request, res: Response) => void
-  ) {
-    const routeKey = `${method}:${this.basePath}${path}`;
-    this.routes[routeKey] = handler;
+  private addRoute(method: Method, path: string, handler: Handler) {
+    this.routes.push({ method, path: `${this.basePath}${path}`, handler });
   }
 
-  // Método para agregar múltiples rutas
-  addRoutes(
-    method: Method,
-    routes: { [path: string]: (req: Request, res: Response) => void }
-  ) {
+  useMiddleware(middleware: Handler) {
+    this.middlewares.push(middleware);
+  }
+
+  addRoutes(method: Method, routes: { [path: string]: Handler }) {
     for (const [path, handler] of Object.entries(routes)) {
       this.addRoute(method, path, handler);
     }
   }
 
-  get(path: string, handler: (req: Request, res: Response) => void) {
+  get(path: string, handler: Handler) {
     this.addRoute("GET", path, handler);
   }
 
-  post(path: string, handler: (req: Request, res: Response) => void) {
+  post(path: string, handler: Handler) {
     this.addRoute("POST", path, handler);
   }
 
-  delete(path: string, handler: (req: Request, res: Response) => void) {
+  delete(path: string, handler: Handler) {
     this.addRoute("DELETE", path, handler);
   }
 
-  put(path: string, handler: (req: Request, res: Response) => void) {
+  put(path: string, handler: Handler) {
     this.addRoute("PUT", path, handler);
   }
 
-  patch(path: string, handler: (req: Request, res: Response) => void) {
+  patch(path: string, handler: Handler) {
     this.addRoute("PATCH", path, handler);
   }
 
   use(prefix: string, router: Router) {
-    const prefixedRouter = new Router(`${this.basePath}${prefix}`);
-    for (const [key, handler] of Object.entries(router.routes)) {
-      prefixedRouter.routes[key] = handler;
-    }
-    this.routes = { ...this.routes, ...prefixedRouter.routes };
+    router.basePath = `${this.basePath}${prefix}`;
+    this.routes.push(...router.routes);
   }
 
-  handleRequest(req: Request, res: Response) {
-    const [path] = req.url.split("?");
-    const routeKey = `${req.method}:${path}`;
-    const handler = this.routes[routeKey];
+  private async executeMiddlewares(req: Request, res: Response) {
+    for (const middleware of this.middlewares) {
+      await middleware(req, res);
+      if (res.headersSent) return; // Stop middleware chain if response has been sent
+    }
+  }
 
-    // Si existen query parameters, parsearlos
+  private findRoute(
+    method: Method,
+    path: string
+  ): { handler: Handler; params: Record<string, string> } | undefined {
+    for (const route of this.routes) {
+      if (route.method === method) {
+        const matchFn: MatchFunction<Record<string, string>> = match(
+          route.path
+        );
+        const matchResult = matchFn(path);
+
+        if (matchResult) {
+          return { handler: route.handler, params: matchResult.params };
+        }
+      }
+    }
+    return undefined;
+  }
+
+  async handleRequest(req: Request, res: Response) {
+    await this.executeMiddlewares(req, res);
+    if (res.headersSent) return;
+
+    const [path] = req.url.split("?");
+    const method = req.method as Method;
+    const route = this.findRoute(method, path);
+
+    if (route) {
+      req.params = route.params;
+      this.parseQueryParams(req);
+      route.handler(req, res);
+    } else {
+      this.handleNotFound(req, res);
+    }
+  }
+
+  private parseQueryParams(req: Request) {
     const queryString = req.url.split("?")[1];
     if (queryString) {
       const queryParams = new URLSearchParams(queryString);
@@ -78,11 +106,9 @@ export class Router {
         req.query[key] = value;
       });
     }
+  }
 
-    if (handler) {
-      handler(req, res);
-    } else {
-      res.status(404).send("Not Found");
-    }
+  private handleNotFound(req: Request, res: Response) {
+    res.status(404).send("Not Found");
   }
 }
