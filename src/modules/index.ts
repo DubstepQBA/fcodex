f8c84ab9a7e7802690e1c6d347495f341fef4300
Javier alfar <dubstepalfaro@gmail.com>
2024-08-30 20:10:42 -0400

    implement functions for jwt

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,3 +1,4 @@
 export { validateMiddleware as validate } from "./validator/validateMiddleware";
 export { logger } from "./logger/loggerMiddleware";
 export { corsMiddleware as cors } from "./cors/corsMiddleware";
+export { configureAuth, generateJWT, verifyJWT } from "./jwt";
